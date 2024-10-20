18d69cdd6941924e26f0f4ae41e706a076f91de4
Kedr237 <ll.vanya237@gmail.com>
2024-10-21 00:19:37 +0300

    Add api documentation

--- src/notes/api/v1/views.py
+++ src/notes/api/v1/views.py
@@ -7,6 +7,9 @@ from .serializers import NoteSerializer
 
 
 class NoteViewSet(viewsets.ModelViewSet):
+    '''
+    Needs "Authorization" header in format: Bearer {token}
+    '''
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
--- src/settings/urls.py
+++ src/settings/urls.py
@@ -1,6 +1,20 @@
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Scrap API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('api/', include('users.urls')),
     path('api/', include('notes.api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
